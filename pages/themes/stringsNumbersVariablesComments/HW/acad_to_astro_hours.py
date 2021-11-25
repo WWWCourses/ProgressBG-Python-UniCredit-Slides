d7cea00cef97aa023000e60ea0b0adadad7a8d30
WWWCourses <ivawebcourses@gmail.com>
2021-11-25 16:04:19 +0200

    update

--- pages/themes/stringsNumbersVariablesComments/HW/acad_to_astro_hours.py
+++ pages/themes/stringsNumbersVariablesComments/HW/acad_to_astro_hours.py
@@ -1,2 +1,15 @@
-# Solution without using variables to store values
-print((64*40 + 64/4*20)/60)
+# variables declaration
+minutes_in_astro_hour = 60
+minutes_in_acad_hour = 40
+course_total_acad_hours = 64
+acad_hours_in_a_day = 3
+minutes_in_break = 15
+
+# program logic:
+course_days = course_total_acad_hours/acad_hours_in_a_day
+total_breaks_minutes = course_days * minutes_in_break
+
+total_course_minutes = course_total_acad_hours * minutes_in_acad_hour + total_breaks_minutes
+total_course_astro_hours = total_course_minutes / minutes_in_astro_hour
+
+print(f'Total course astronomical hours are: {total_course_astro_hours}')
