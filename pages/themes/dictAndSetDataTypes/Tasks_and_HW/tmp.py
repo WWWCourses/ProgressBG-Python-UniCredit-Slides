2c9aab58ef954ae43d9d2719d477ee8f3c219814
WWWCourses <ivawebcourses@gmail.com>
2021-12-14 16:10:53 +0200

    update HW with more hints; fix ids

--- pages/themes/dictAndSetDataTypes/Tasks_and_HW/tmp.py
+++ pages/themes/dictAndSetDataTypes/Tasks_and_HW/tmp.py
@@ -1,4 +1,13 @@
-text = "some words delimited by spaces"
-words_list = text.split()
+# numbers = [2,3,1,5]
 
-print(words_list)
+# min_number = min(numbers)
+# max_number = max(numbers)
+
+
+x = 2
+y = 5
+min_number = min(x,y)
+max_number = max(x,y)
+
+print(min_number)
+print(max_number)
