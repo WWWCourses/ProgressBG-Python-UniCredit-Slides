2042868e63b63c0a5a66edd605385cc988aef46c
WWWCourses <ivawebcourses@gmail.com>
2022-03-25 12:20:21 +0200

    update

--- pages/themes/usingTheFilesystem/examples/file_manipulation.py
+++ pages/themes/usingTheFilesystem/examples/file_manipulation.py
@@ -1,7 +1,36 @@
-filename = "test_file.txt"
+import os
 
-fh = open(filename, "r")
-print(dir(fh))
+# filename = "test_file.txt"
 
-for line in fh.readlines():
- 	print(line, end="")
+# fh = open(filename, "r")
+# print(dir(fh))
+
+# for line in fh.readlines():
+#  	print(line, end="")
+
+
+# while line=fh.readline():
+# 	print(line)
+
+
+# print(os.getcwd())
+
+# os.chdir("/media/nemsys/data/projects/courses/ProgressBG/ProgressBG-Python-UniCredit/ProgressBG-Python-UniCredit-Slides/pages/themes/usingTheFilesystem")
+# print(os.getcwd())
+
+
+# filename = "test_file.txt"
+
+# fh = open('test_file.txt', 'r')
+# # for l in fh:
+# # 	print(l, end='')
+
+# for l in fh.readlines():
+# 	print(l, end='')
+
+
+# os.remove('test_file.txt')
+
+# with open('test_file.txt', 'r') as fh:
+# 	for l in fh:
+# 		print(l, end='')
